c08ff2e30f3024e7367b82e1a7e2338a60d5845a
Crevan <a.siverskii@gmail.com>
2023-06-08 22:48:25 +0300

    [update] MealsUtil

--- src/main/java/com/crevan/manager/util/MealsUtil.java
+++ src/main/java/com/crevan/manager/util/MealsUtil.java
@@ -4,45 +4,39 @@ import com.crevan.manager.model.Meal;
 import com.crevan.manager.model.MealTo;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.time.Month;
-import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class MealsUtil {
 
-    public static void main(String[] args) {
-        List<Meal> meals = Arrays.asList(
-                new Meal(500, "Завтрак", LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0)),
-                new Meal(1000, "Обед", LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0)),
-                new Meal(500, "Ужин", LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0)),
-                new Meal(100, "Еда на граничное значение", LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0)),
-                new Meal(1000, "Завтрак", LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0)),
-                new Meal(500, "Обед", LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0)),
-                new Meal(410, "Ужин", LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0))
-        );
+    public static final int DEFAULT_CALORIES_COUNT = 2000;
 
-        List<MealTo> mealsTo = filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
-        mealsTo.forEach(System.out::println);
+    public static List<MealTo> getTos(Collection<Meal> meals, int caloriesPerDay) {
+        return filterByPredicate(meals, caloriesPerDay, meal -> true);
     }
 
-    public static List<MealTo> filteredByStreams(List<Meal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+    public static List<MealTo> getFilteredTos(Collection<Meal> meals, int caloriesPerDay, LocalTime startTime, LocalTime endTime) {
+        return filterByPredicate(meals, caloriesPerDay, meal -> DateTimeUtil.isBetweenHalfOpen(meal.getTime(), startTime, endTime));
+    }
+
+    private static List<MealTo> filterByPredicate(Collection<Meal> meals, int caloriesPerDay, Predicate<Meal> predicate) {
         Map<LocalDate, Integer> caloriesSumByDate = meals.stream()
                 .collect(
                         Collectors.groupingBy(Meal::getDate, Collectors.summingInt(Meal::getCalories))
-//                      Collectors.toMap(Meal::getDate, Meal::getCalories, Integer::sum)
+//                        Collectors.toMap(Meal::getDate, Meal::getCalories, Integer::sum)
                 );
 
         return meals.stream()
-                .filter(meal -> TimeUtil.isBetweenHalfOpen(meal.getTime(), startTime, endTime))
+                .filter(predicate)
                 .map(meal -> createTo(meal, caloriesSumByDate.get(meal.getDate()) > caloriesPerDay))
                 .collect(Collectors.toList());
     }
 
     private static MealTo createTo(Meal meal, boolean excess) {
-        return new MealTo(meal.getCalories(), meal.getDescription(), meal.getDateTime(), excess);
+        return new MealTo(meal.getId(), meal.getCalories(), meal.getDescription(), meal.getDateTime(), excess);
     }
 }
