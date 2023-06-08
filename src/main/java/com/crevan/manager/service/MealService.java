7162c1fbf08a0bcb7450cf193f06125fe0c73088
Crevan <a.siverskii@gmail.com>
2023-06-08 22:46:31 +0300

    [add] create service layer

---
+++ src/main/java/com/crevan/manager/service/MealService.java
@@ -0,0 +1,8 @@
+package com.crevan.manager.service;
+
+import com.crevan.manager.repository.MealRepository;
+
+public class MealService {
+
+    private MealRepository repository;
+}
---
+++ src/main/java/com/crevan/manager/service/UserService.java
@@ -0,0 +1,38 @@
+package com.crevan.manager.service;
+
+import com.crevan.manager.model.User;
+import com.crevan.manager.repository.UserRepository;
+
+import java.util.List;
+
+import static com.crevan.manager.util.ValidationUtil.checkNotFound;
+import static com.crevan.manager.util.ValidationUtil.checkNotFoundWithId;
+
+public class UserService {
+
+    private UserRepository repository;
+
+    public User create(final User user) {
+        return repository.save(user);
+    }
+
+    public void delete(final int id) {
+        checkNotFoundWithId(repository.delete(id), id);
+    }
+
+    public User get(final int id) {
+        return checkNotFoundWithId(repository.get(id), id);
+    }
+
+    public User getByEmail(final String email) {
+        return checkNotFound(repository.getByEmail(email), "email=" + email);
+    }
+
+    public List<User> getAll() {
+        return repository.getAll();
+    }
+
+    public void update(final User user) {
+        checkNotFoundWithId(repository.save(user), user.getId());
+    }
+}
