48ed14317b62d2bf3c4993b291fb49240a17d0d6
Crevan <a.siverskii@gmail.com>
2023-06-08 22:44:33 +0300

    [add] BaseEntity classes

---
+++ src/main/java/com/crevan/manager/model/AbstractBaseEntity.java
@@ -0,0 +1,27 @@
+package com.crevan.manager.model;
+
+public class AbstractBaseEntity {
+
+    protected Integer id;
+
+    protected AbstractBaseEntity(final Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(final Integer id) {
+        this.id = id;
+    }
+
+    public boolean isNew() {
+        return this.id == null;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + ":" + id;
+    }
+}
