ba30f386f12a376cb1695699e5b4e5d4cde8b3aa
a.siverskii <a.siverskii@gmail.com>
2023-07-16 20:10:31 +0300

    [update] profiles connection pool

---
+++ src/main/java/com/crevan/manager/Profiles.java
@@ -0,0 +1,16 @@
+package com.crevan.manager;
+
+public class Profiles {
+
+    public static final String
+            JDBC = "JDBC",
+            JPA = "JPA";
+
+    public static final String REPOSITORY_IMPLEMENTATION = JPA;
+
+    public static final String
+            POSTGRES_DB = "postgres",
+            HSQL_DB = "hsqldb";
+
+    public static final String ACTIVE_DB = POSTGRES_DB;
+}
