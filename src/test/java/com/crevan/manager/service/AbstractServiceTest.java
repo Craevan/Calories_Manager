4686e1d20119cfe39acd1a84c2dfae1424261056
a.siverskii <a.siverskii@gmail.com>
2023-07-23 23:29:33 +0300

    [update] extract rules

--- src/test/java/com/crevan/manager/service/AbstractServiceTest.java
+++ src/test/java/com/crevan/manager/service/AbstractServiceTest.java
@@ -1,21 +1,17 @@
 package com.crevan.manager.service;
 
 import com.crevan.manager.ActiveDbProfileResolver;
-import org.junit.AfterClass;
+import com.crevan.manager.TimingRule;
+import org.junit.ClassRule;
 import org.junit.Rule;
+import org.junit.rules.ExternalResource;
 import org.junit.rules.Stopwatch;
-import org.junit.runner.Description;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.concurrent.TimeUnit;
-
-import static org.slf4j.LoggerFactory.getLogger;
 
 @ContextConfiguration({
         "classpath:spring/spring-app.xml",
@@ -26,27 +22,9 @@ import static org.slf4j.LoggerFactory.getLogger;
 @ActiveProfiles(resolver = ActiveDbProfileResolver.class)
 public abstract class AbstractServiceTest {
 
-    private static final Logger log = getLogger("result");
-    private static final StringBuilder results = new StringBuilder();
+    @ClassRule
+    public static ExternalResource summary = TimingRule.SUMMARY;
 
     @Rule
-    public final Stopwatch stopwatch = new Stopwatch() {
-        @Override
-        protected void finished(final long nanos, final Description description) {
-            String result = String.format("\n%-25s %7d", description.getMethodName(), TimeUnit.NANOSECONDS.toMillis(nanos));
-            results.append(result);
-            log.info(result + "ms\n");
-        }
-    };
-
-    //    https://dzone.com/articles/applying-new-jdk-11-string-methods
-    private static final String DELIM = "-".repeat(103);
-
-    @AfterClass
-    public static void printResult() {
-        log.info("\n" + DELIM +
-                "\nTest                                                                                       Duration, ms" +
-                "\n" + DELIM + "\n" + results + DELIM + "\n");
-        results.setLength(0);
-    }
+    public Stopwatch stopwatch = TimingRule.STOPWATCH;
 }
