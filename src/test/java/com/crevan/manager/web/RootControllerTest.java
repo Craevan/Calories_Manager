e537a9cbd573345b87896774359c7b65ece3e28a
a.siverskii <a.siverskii@gmail.com>
2023-08-25 22:07:05 +0300

    [add] controller test

---
+++ src/test/java/com/crevan/manager/web/RootControllerTest.java
@@ -0,0 +1,28 @@
+package com.crevan.manager.web;
+
+import com.crevan.manager.UserTestData;
+import org.junit.Test;
+
+import static com.crevan.manager.model.AbstractBaseEntity.START_SEQ;
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+public class RootControllerTest extends AbstractControllerTest {
+
+    @Test
+    public void getUser() throws Exception {
+        perform(get("/users"))
+                .andDo(print())
+                .andExpect(view().name("users"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/users.jsp"))
+                .andExpect(model().attribute("users", hasSize(3)))
+                .andExpect(model().attribute("users", hasItem(
+                        allOf(
+                                hasProperty("id", is(START_SEQ)),
+                                hasProperty("name", is(UserTestData.user.getName()))
+                        )
+                )));
+    }
+}
